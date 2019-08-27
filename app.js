8ee4aac65cb05c503a4615e09d87ccead89f8de0
ShaharAssenehim <shacharassen3667@gmail.com>
2019-08-27 15:03:32 +0300

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var commentRoutes    = require("./routes/comments"),
     indexRoutes      = require("./routes/index")
     
 //mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser:true});
-mongoose.connect("mongodb+srv://shaharassen:shachar222@cluster0-cv4kt.mongodb.net/test?retryWrites=true&w=majority",{
+mongoose.connect("mongodb+srv://shaharassen:shachar222@cluster0-cv4kt.mongodb.net/EscapeRoom?retryWrites=true&w=majority",{
     useNewUrlParser:true,
     useCreateIndex: true
 }).then(()=>{
